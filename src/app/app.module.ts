c19e7a5902f5d99b3ff8e8d4ceb8a983446523b3
hehSooC <prigenius7@gmail.com>
2018-03-14 17:50:27 -0400

    Midterm_RoutePages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { HomeComponent } from './home/home.component';
+import { MessagesComponent } from './messages/messages.component';
+import { ExerciseComponent } from './exercise/exercise.component';
+
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    HomeComponent
+    HomeComponent,
+    MessagesComponent,
+    ExerciseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: 'home', component: HomeComponent},
+      { path: 'exercise', component: ExerciseComponent},
+      { path: '', redirectTo: '/home', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/messages/messages.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-messages',
+  templateUrl: './messages.component.html',
+  styleUrls: ['./messages.component.css']
+})
+export class MessagesComponent implements OnInit {
+
+    Messages: string[];
+  constructor() { 
+    this.Messages = ['Record Your Daily Exercise!'];
+  }
+
+  ngOnInit() {
+  }
+
+}
