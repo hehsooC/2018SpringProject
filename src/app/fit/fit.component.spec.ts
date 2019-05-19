dc13658e5003592ca54b58e42ec0f4f499d7f038
zero_hs <prigenius7@gmail.com>
2019-05-19 00:10:01 -0400

    package json add info & home add warning

--- src/app/fit/fit.component.spec.ts
+++ src/app/fit/fit.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { GameComponent } from './game.component';
+import { FitComponent } from './fit.component';
 
-describe('GameComponent', () => {
-  let component: GameComponent;
-  let fixture: ComponentFixture<GameComponent>;
+describe('FitComponent', () => {
+  let component: FitComponent;
+  let fixture: ComponentFixture<FitComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ GameComponent ]
+      declarations: [ FitComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(GameComponent);
+    fixture = TestBed.createComponent(FitComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
